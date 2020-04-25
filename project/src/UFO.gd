5765504474e9323f570a556ece072b72a26c53f2
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-04-25 13:11:46 -0400

    Add enemy explode sound when hit

--- project/src/UFO.gd
+++ project/src/UFO.gd
@@ -1,8 +1,20 @@
 extends PathFollow2D
 
 export var speed = 100
+var dead = false
+onready var explosion = $Boom
 
 func _process(delta):
-	offset += speed*delta
-	if unit_offset > 1:
-		queue_free()
+	if dead == false:
+		offset += speed*delta
+		if unit_offset > 1:
+			queue_free()
+
+func _on_body_entered(body):
+	if body.is_in_group("Bullet"):
+		if dead == false:
+			explosion.play()
+			dead = true
+			body.queue_free()
+			yield(get_tree().create_timer(0.5), "timeout")
+			queue_free()
