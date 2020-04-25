8b88f8c6370ac0e76938606a07c365267a6486e5
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-04-25 14:17:17 -0400

    Add missing enemy bullet and test scene

---
+++ project/src/EnemyBullet.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+export var velocity : Vector2
+
+func _process(delta: float):
+	var collision :KinematicCollision2D = move_and_collide(velocity*delta)
+	if collision:
+		var collider : Node2D = collision.collider as Node2D
+		if collider.is_in_group("LevelBounds"):
+			queue_free()
+		elif collider is Rocket:
+			(collider as Rocket).damage()
+			queue_free()
+
+func _draw():
+	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.pink)
