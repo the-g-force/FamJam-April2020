1c04cc2ad28239c889fe1eb839ea9159ec7db07f
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-04-25 17:42:56 -0400

    Remove compiler warnings

--- project/src/End.gd
+++ project/src/End.gd
@@ -8,12 +8,15 @@ var _level : PackedScene = load("res://src/Level.tscn")
 func _ready():
 	_scoredisplay.text = "Your score was:" + str(PlayerStats.score)
 
+
 func _on_Restart_pressed():
-	get_tree().change_scene_to(_level)
 	PlayerStats.health = 10
 	PlayerStats.score = 0
+	var _error = get_tree().change_scene_to(_level)
+
 
 func _on_Main_pressed():
-	get_tree().change_scene_to(_main)
 	PlayerStats.health = 10
 	PlayerStats.score = 0
+	var _error = get_tree().change_scene_to(_main)
+
