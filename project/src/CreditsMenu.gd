6fd1c36a80bd2e04bf65de5e26a3fd52308bb86a
Jessica Gestwicki <gestwicki@gmail.com>
2020-04-25 17:13:09 -0400

    Add main menu and about screens

---
+++ project/src/CreditsMenu.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_BackButton_pressed():
+	var _error = get_tree().change_scene("res://src/MainMenu.tscn")
--- project/src/MainMenu.gd
+++ project/src/MainMenu.gd
@@ -1,6 +1,13 @@
 extends Control
 
-var _Level : PackedScene = preload("res://src/Level.tscn")
+const _Level : PackedScene = preload("res://src/Level.tscn")
+
+const _Credits : PackedScene = preload("res://src/CreditsMenu.tscn")
+
 
 func _on_StartButton_pressed():
 	var _error = get_tree().change_scene_to(_Level)
+
+
+func _on_CreditsButton_pressed():
+	var _error = get_tree().change_scene_to(_Credits) # Replace with function body.
