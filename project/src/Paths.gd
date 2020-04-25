bf976cd505426822b838685dbfa07b79b73679e5
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-04-25 13:29:29 -0400

    Refactor the UFO to be an area
    The Paths object is now responsible for pathfollowing logic

--- project/src/Paths.gd
+++ project/src/Paths.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
+export var ufo_speed : float = 100
+
 var enemy = preload("res://src/UFO.tscn")
+
 onready var paths = $Paths
-var path
 
 func _on_Timer_timeout():
-	randomize()
-	var UFO = enemy.instance()
-	path = paths.get_children()[randi () % paths.get_child_count()]
-	path.add_child(UFO)
+	var ufo = enemy.instance()
+	var path_follow = PathFollow2D.new()
+	path_follow.add_child(ufo)
+	
+	var path = paths.get_children()[randi () % paths.get_child_count()]
+	path.add_child(path_follow)
+
+func _process(delta:float):
+	for p in $Paths.get_children():
+		var path = p as Path2D
+		for ufo_path_follow in path.get_children():
+			ufo_path_follow.offset += ufo_speed * delta
--- project/src/UFO.gd
+++ project/src/UFO.gd
@@ -1,20 +1,16 @@
-extends PathFollow2D
+extends Area2D
 
 export var speed = 100
-var dead = false
-onready var explosion = $Boom
 
-func _process(delta):
-	if dead == false:
-		offset += speed*delta
-		if unit_offset > 1:
-			queue_free()
+var _dead = false
+
+onready var explosion = $Boom
 
 func _on_body_entered(body):
 	if body.is_in_group("Bullet"):
-		if dead == false:
+		if _dead == false:
 			explosion.play()
-			dead = true
+			_dead = true
 			body.queue_free()
 			yield(get_tree().create_timer(0.5), "timeout")
 			queue_free()
