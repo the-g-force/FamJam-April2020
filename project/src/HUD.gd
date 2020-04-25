5db0d807d4a5d999a7e97d4b86f78ec20d1f80ac
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-04-25 17:12:59 -0400

    Remove compiler warning

--- project/src/HUD.gd
+++ project/src/HUD.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 onready var healthtracker = $HBoxContainer/Health
 onready var scoretracker = $HBoxContainer/Score
 
-func _process(delta):
+func _process(_delta):
 	if PlayerStats.health >= 0:
 		healthtracker.text = "health:" + str(PlayerStats.health)
 	scoretracker.text = "score:" + str(PlayerStats.score) + " "
