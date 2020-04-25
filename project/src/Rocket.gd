dc8b9b22e3abd6c06b58b4218b15c065d7b9801d
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-04-25 16:11:40 -0400

    Add player explosion

--- project/src/Rocket.gd
+++ project/src/Rocket.gd
@@ -11,47 +11,58 @@ export var max_speed : float = 150
 export var Bullet = preload("res://src/Bullet.tscn")
 export var bullet_speed : float = 400
 
-var _speed : float = 0
-
 onready var _flames : Node2D = $Hull/Flames
+onready var _collision : CollisionShape2D = $CollisionShape2D
 onready var _gunpoint : Node2D = $GunPoint
 onready var _shoot_sound : AudioStreamPlayer2D = $ShootSound
 onready var _thrust_sound = $ThrustSound
 
+var _speed : float = 0
+var _dead : bool = false
+
+const _explosion = preload("res://src/Explosion.tscn")
+
 func _process(delta):
-	# Turn according to the rotation inputs
-	rotation_degrees += \
-			(-Input.get_action_strength("TurnLeft") + Input.get_action_strength("TurnRight")) \
-			* rotation_speed \
-			* delta
+	if not _dead:
+		# Turn according to the rotation inputs
+		rotation_degrees += \
+				(-Input.get_action_strength("TurnLeft") + Input.get_action_strength("TurnRight")) \
+				* rotation_speed \
+				* delta
+				
+		# Play thrust sound as needed
+		if Input.get_action_strength("Thrust") != 0:
+			if _thrust_sound.playing == false:
+				_thrust_sound.play()
+		else:
+			_thrust_sound.stop()
 			
-	# Play thrust sound as needed
-	if Input.get_action_strength("Thrust") != 0:
-		if _thrust_sound.playing == false:
-			_thrust_sound.play()
-	else:
-		_thrust_sound.stop()
+		# Determine the speed based on acceleration
+		var target_speed = lerp(min_speed, max_speed, Input.get_action_strength("Thrust"))
+		_speed = lerp(_speed, target_speed, acceleration)
 		
-	# Determine the speed based on acceleration
-	var target_speed = lerp(min_speed, max_speed, Input.get_action_strength("Thrust"))
-	_speed = lerp(_speed, target_speed, acceleration)
+		# Move the rocket
+		var velocity : Vector2 = Vector2(0, -_speed).rotated(deg2rad(rotation_degrees)) * delta
+		var _collision = move_and_collide(velocity) # Ignore the collision results for now
 	
-	# Move the rocket
-	var velocity : Vector2 = Vector2(0, -_speed).rotated(deg2rad(rotation_degrees)) * delta
-	var _collision = move_and_collide(velocity) # Ignore the collision results for now
+		# Update the flame visibility
+		_flames.visible = Input.get_action_strength("Thrust") > 0
 
-	# Update the flame visibility
-	_flames.visible = Input.get_action_strength("Thrust") > 0
-
-	# Fire!
-	if Input.is_action_just_pressed("Fire"):
-		var bullet = Bullet.instance()
-		bullet.forward_vector = Vector2(0,-1).rotated(rotation)
-		bullet.position = $GunPoint.get_global_transform().get_origin()
-		get_parent().add_child(bullet)
-		_shoot_sound.play()
+		# Fire!
+		if Input.is_action_just_pressed("Fire"):
+			var bullet = Bullet.instance()
+			bullet.forward_vector = Vector2(0,-1).rotated(rotation)
+			bullet.position = $GunPoint.get_global_transform().get_origin()
+			get_parent().add_child(bullet)
+			_shoot_sound.play()
 
 func damage():
 	PlayerStats.health -= 1
 	if PlayerStats.health <= 0:
-		print("Died")
+		if not _dead:
+			var _explode = _explosion.instance()
+			_explode.position = get_global_transform().origin
+			get_tree().get_root().call_deferred('add_child', _explode)
+			hide()
+			_collision.disabled = true
+			_dead = true
