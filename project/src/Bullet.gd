6d649142588c1c25f48c4b540e9b3e270c4171f7
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-04-25 12:52:55 -0400

    Redesign bullet to work with levels and masks

--- project/src/Bullet.gd
+++ project/src/Bullet.gd
@@ -1,17 +1,17 @@
-extends Area2D
+extends KinematicBody2D
 
 export var velocity : Vector2
 
 func _process(delta: float):
-	position += velocity * delta
+	var collision :KinematicCollision2D = move_and_collide(velocity*delta)
+	if collision:
+		var collider : Node2D = collision.collider as Node2D
+		if collider.is_in_group("LevelBounds"):
+			queue_free()
+	
 
 
 func _draw():
 	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.yellow)
 
-
-func _on_Bullet_body_entered(body):
-	
-	if body.is_in_group("LevelBounds"):
-		queue_free()
 
