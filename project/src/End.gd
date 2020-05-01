ffa461215bf09f3bc37738027ef0fca169e0baa2
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-05-01 16:26:50 -0400

    Use audible button in end scene

--- project/src/End.gd
+++ project/src/End.gd
@@ -9,16 +9,16 @@ func _ready():
 	_scoredisplay.text = "You saved " + str(PlayerStats.score) + " chickens!"
 
 
-func _on_Restart_pressed():
-	Jukebox.pressed.play()
-	PlayerStats.health = 10
-	PlayerStats.score = 0
+func _on_Restart_audio_feedback_complete():
+	_reset_player_stats()
 	var _error = get_tree().change_scene_to(_level)
 
 
-func _on_Main_pressed():
-	Jukebox.pressed.play()
-	PlayerStats.health = 10
-	PlayerStats.score = 0
+func _on_Main_audio_feedback_complete():
+	_reset_player_stats()
 	var _error = get_tree().change_scene_to(_main)
 
+
+func _reset_player_stats():
+	PlayerStats.health = 10
+	PlayerStats.score = 0
