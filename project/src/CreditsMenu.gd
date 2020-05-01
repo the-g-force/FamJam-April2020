2f0489b55c4ad9045b0ebfd6808bb3a96ce76731
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-05-01 16:14:19 -0400

    Moved button sound responsibility into credits menu

--- project/src/CreditsMenu.gd
+++ project/src/CreditsMenu.gd
@@ -1,5 +1,8 @@
 extends Control
 
 func _on_BackButton_pressed():
-	Jukebox.pressed.play()
+	$ButtonClickSound.play()
+
+
+func _on_ButtonClickSound_finished():
 	var _error = get_tree().change_scene("res://src/MainMenu.tscn")
